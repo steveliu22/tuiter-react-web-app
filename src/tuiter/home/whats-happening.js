869ac9325eafb0a50cc69dd410ed9b0817c2c054
steveliu22 <liu.steve@northeastern.edu>
2022-11-19 22:42:50 -0500

    fixed tuits issue

--- src/tuiter/home/whats-happening.js
+++ src/tuiter/home/whats-happening.js
@@ -26,7 +26,7 @@ const WhatsHappening = () => {
        </textarea>
        <div>
          <button className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
-                 onClick={tuitClickHandler}>
+                 onClick={() => tuitClickHandler(whatsHappening)}>
            Tuit
          </button>
          <div className="text-primary fs-2">
